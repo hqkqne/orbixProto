e99d78d76544200403cdbe414dba3ba7baeecc48
f1to <hqkqne@gmail.com>
2026-04-14 22:15:56 +0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt6.QtCore import QSize,Qt
+from PyQt6.QtWidgets import (
+QApplication,QPushButton, QTextEdit, QMainWindow, QCheckBox, QTreeWidget
+)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("application")
+        self.setMinimumSize(QSize(400, 300))
+        self.setMaximumSize(QSize(800,600))
+
+        button = QPushButton('text')
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.is_toggled)
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(button):
+        print('Click!')
+    def is_toggled(self, checked):
+        print("click?", checked)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
