38f171cd036dc051e59fe376fb2107d3fa90bd6f
hqkqne <hqkqne@gmail.com>
2026-04-17 18:11:36 +0300

    WIP ui->py

---
+++ src/auth_dialog.py
@@ -0,0 +1,28 @@
+from PyQt6.QtWidgets import QDialog, QMessageBox
+from PyQt6.QtCore import pyqtSignal
+from ui.ui_login import Ui_Dialog as Ui_login
+from ui.ui_reg import Ui_Dialog as Ui_reg
+
+class LoginDialog(QDialog, Ui_login):
+    login_successful = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.login_btn.clicked.connect(self.handle_login)
+        self.reg_btn.mousePressEvent = lambda e: self.show_register()
+        #визуально отличимая
+        self.reg_btn.setStyleSheet("color: #0078D4; text_decoration: underline; cursor: pointer;")
+
+    def handle_login(self):
+        email = self.email_edit.text().strip()
+        pwd = self.password_edit.text().strip()
+        if not email or not pwd:
+            QMessageBox.warning(self,"error", "login and password are required")
+            return
+        # self.login_successful.emit(email)
+        self.accept()
+
+    def show_register(self, event = None):
+        reg =
